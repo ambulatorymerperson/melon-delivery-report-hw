f04abd8e03f9fcfbe871981ae947a640ed6dd959
Isabella Applen <isabellaapplen@gmail.com>
2018-04-05 04:55:20 +0000

    condensed file into one function

--- produce_summary.py
+++ produce_summary.py
@@ -7,14 +7,14 @@ def printing_produce_orders(day, txt):
         line = line.rstrip()
         words = line.split('|')
 
-        melon = words[0]
-        count = words[1]
-        amount = words[2]
+        # melon = words[0]
+        # count = words[1]
+        # amount = words[2]
 
         print "Delivered {} {}s for total of ${}".format(
-            count, melon, amount)
+            words[0], words[1], words[2])
     the_file.close()
-    
+
 printing_produce_orders("Day 1", "um-deliveries-20140519.txt")
 printing_produce_orders("Day 2", "um-deliveries-20140520.txt")
 printing_produce_orders("Day 3", "um-deliveries-20140521.txt")
